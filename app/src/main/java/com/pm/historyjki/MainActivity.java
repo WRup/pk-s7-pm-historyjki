4de29bef1e9f65db24d98cf2dca079ef07481ff5
Michał Rotko <michalr835@gmail.com>
2018-12-04 12:31:17 +0100

    lombok, upgrade

--- app/src/main/java/com/pm/historyjki/MainActivity.java
+++ app/src/main/java/com/pm/historyjki/MainActivity.java
@@ -2,6 +2,7 @@ package com.pm.historyjki;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import lombok.Data;
 
 public class MainActivity extends AppCompatActivity {
 
